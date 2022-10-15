f5b750f562d0c2d85c4ac4d161b940226f7295c4
vb71021 <vb71021@gmail.com>
2022-10-15 23:15:36 +0300

    Added search engine

---
+++ src/Search.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Search() {
+  const [city, setCity] = useState(null);
+  const [message, setMessage] = useState(null);
+
+  function getWeather(event) {
+    event.preventDefault();
+
+    function showWeather(response) {
+      let iconUrl = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
+      setMessage(
+        <ul>
+          <li>
+            The temperature is {Math.round(response.data.main.temp)}°C in {city}
+          </li>
+          <li>
+            The humidity is {response.data.main.humidity}% in {city}
+          </li>
+          <li>
+            The wind is {Math.round(response.data.wind.speed)} km/h in {city}
+          </li>
+          <li>
+            Description: {response.data.weather[0].description} in {city}
+          </li>
+          <li>
+            <img src={iconUrl} alt={response.data.weather[0].description} />
+          </li>
+        </ul>
+      );
+    }
+    let apiKey = "d9cd27eb3f86fe62cc5c47529385e41c";
+    let units = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(showWeather);
+  }
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  return (
+    <div className="Search">
+      <form onSubmit={getWeather}>
+        <input type="search" placeholder="Type city" onChange={updateCity} />
+        <button type="Submit">Show Weather</button>
+      </form>
+      <div>{message}</div>
+    </div>
+  );
+}
