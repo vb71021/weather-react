2de4c78c682c5d24b2dd824d25c17764804396d7
vb71021 <vb71021@gmail.com>
2022-11-07 23:22:29 +0200

    Removed Forecast

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Search from "./Search";
 
 import "./App.css";
-import Forecast from "./Forecast";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 
@@ -11,10 +10,6 @@ function App() {
       <div className="container">
         <div className="card">
           <Search defaultCity="Kyiv" />
-          <div className="forecast_button">
-            <button>👉Forecast</button>
-          </div>
-          <Forecast />
         </div>
         <footer className="github_link">
           <a href="https://github.com/vb71021/weather-react.git" id="source">
